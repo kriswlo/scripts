c8efd8f9f744d270406b4a1aaaff291adf83d93a
kriswlo <37249477+kriswlo@users.noreply.github.com>
2019-02-28 16:31:08 +0100

    Update ironic.sh

--- ironic.sh
+++ ironic.sh
@@ -1,1 +1,6 @@
-apt install mysql-server
+#!/bin/bash
+sed -i "s/# groot=LABEL=cloudimg-rootfs/# groot=(hd0)/g" /boot/grub/menu.lst
+update-grub-legacy-ec2 
+apt update
+apt upgrade
+apt install mysql-server -y
