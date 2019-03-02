9118dc3290be58c53d8f0c118451a47e33796231
kriswlo <37249477+kriswlo@users.noreply.github.com>
2019-03-02 08:49:28 +0100

    Create bifrost.sh

---
+++ bifrost.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+log='/root/post_install.txt'
+lr=' 2>> '$log
+l=' >> '$log$lr
+eval timedatectl set-timezone Europe/Oslo $l
+cd /root
+eval mkdir /root/.ssh
+eval date $l
+eval "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9Rv7mjUkhhKAXp/FORWCLoKtr3Wkp+CL8e+FfsG0sGcGIDHPaRJ2mHoxpnIeqMKsYKnq5VeOqhEUlNYnjehrRayrUixCNWiO1ZPAilj0DgjBnykOJq40Qn+FFtRT5jks4Jd+kiXtMnhUcEB5/jx2DBYApEYmTa0fsJtmh6WhXRfQAPioISOQZtaEMAxSrwTOGjldp7Z+qjEDt6DySTBozfpIPDvzY/qBAn1HaD7NwLN2/4PoBrbw4kp74APCDciW1ynkVIFoelzoAAWo73ZFq5PdhEq01dkY8a8DM11OFWlW0o7EnypscIZtD1dzxHPlj7h/+usSf3uXPBRc18F3x kris@Krzysztofs-MBP' >> /root/.ssh/authorized_keys $lr"
+eval chmod 600 /root/.ssh $l
+eval chmod 400 /root/.ssh/authorized_keys $l
+sed -i "s/# groot=LABEL=cloudimg-rootfs/# groot=(hd0)/g" /boot/grub/menu.lst
+eval update-grub-legacy-ec2 $l
+export DEBIAN_FRONTEND=noninteractive
+export DEBIAN_PRIORITY=critical
+sudo -E apt-get -qy update
+sudo -E apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
+sudo -E apt-get -qy autoclean
