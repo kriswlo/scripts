d792ea80af3834b87773017c6cea568eb5590a72
kriswlo <37249477+kriswlo@users.noreply.github.com>
2019-03-03 21:15:06 +0100

    Update bifrost.sh

--- bifrost.sh
+++ bifrost.sh
@@ -2,7 +2,7 @@
 log='/root/post_install.txt'
 lr=' 2>> '$log
 l=' >> '$log$lr
-eval echo "before firewall" $l
+eval echo "Starting - before firewall" $l
 echo '#!/bin/bash
 iptables -F
 iptables -P INPUT DROP
@@ -15,7 +15,7 @@ chmod 755 /root/rc.firewall
 eval timedatectl set-timezone Europe/Oslo $l
 eval date $l
 eval mkdir /root/.ssh
-eval "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9Rv7mjUkhhKAXp/FORWCLoKtr3Wkp+CL8e+FfsG0sGcGIDHPaRJ2mHoxpnIeqMKsYKnq5VeOqhEUlNYnjehrRayrUixCNWiO1ZPAilj0DgjBnykOJq40Qn+FFtRT5jks4Jd+kiXtMnhUcEB5/jx2DBYApEYmTa0fsJtmh6WhXRfQAPioISOQZtaEMAxSrwTOGjldp7Z+qjEDt6DySTBozfpIPDvzY/qBAn1HaD7NwLN2/4PoBrbw4kp74APCDciW1ynkVIFoelzoAAWo73ZFq5PdhEq01dkY8a8DM11OFWlW0o7EnypscIZtD1dzxHPlj7h/+usSf3uXPBRc18F3x kris@Krzysztofs-MBP' >> /root/.ssh/authorized_keys $lr"
+eval "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9Rv7mjUkhhKAXp/FORWCLoKtr3Wkp+CL8e+FfsG0sGcGIDHPaRJ2mHoxpnIeqMKsYKnq5VeOqhEUlNYnjehrRayrUixCNWiO1ZPAilj0DgjBnykOJq40Qn+FFtRT5jks4Jd+kiXtMnhUcEB5/jx2DBYApEYmTa0fsJtmh6WhXRfQAPioISOQZtaEMAxSrwTOGjldp7Z+qjEDt6DySTBozfpIPDvzY/qBAn1HaD7NwLN2/4PoBrbw4kp74APCDciW1ynkVIFoelzoAAWo73ZFq5PdhEq01dkY8a8DM11OFWlW0o7EnypscIZtD1dzxHPlj7h/+usSf3uXPBRc18F3x kris@Krzysztofs-MBP' > /root/.ssh/authorized_keys $lr"
 eval chmod 600 /root/.ssh $l
 eval chmod 400 /root/.ssh/authorized_keys $l
 cd /root
@@ -35,11 +35,12 @@ eval echo "before rc.local" $l
 echo '#!/bin/bash
 /root/rc.firewall
 apt-get -qy update
-apt upgrade -y
+apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
 apt-get -qy autoclean
 dpkg --configure -a
 apt install python-pip -y
 pip install -U pip ansible softlayer bifrost
+git clone https://git.openstack.org/openstack/bifrost.git
 head -n 2 /etc/rc.local >/etc/rc.local1
 mv /etc/rc.local1 /etc/rc.local
 chmod 755 /etc/rc.local' > /etc/rc.local
