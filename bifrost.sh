9154a8de998bf2814fe521f29c6b54fa1dbb9ca0
kriswlo <37249477+kriswlo@users.noreply.github.com>
2019-03-04 23:32:54 +0100

    Update bifrost.sh

--- bifrost.sh
+++ bifrost.sh
@@ -14,15 +14,16 @@ init() {
  iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
  iptables -A INPUT -s 80.68.235.230 -j ACCEPT
  iptables -A INPUT -s 10.180.0.0/22 -j ACCEPT
- iptables -A INPUT -p udp --dp 67 -j ACCEPT" >/root/rc.firewall
+ iptables -A INPUT -i eth0 -p udp --dp 67 -j ACCEPT
+ iptables -A INPUT -i eth0 -p udp --dp 69  -j ACCEPT" >/root/rc.firewall
  chmod 755 /root/rc.firewall
  /root/rc.firewall
  eval mkdir /root/.ssh $l
  eval "echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9Rv7mjUkhhKAXp/FORWCLoKtr3Wkp+CL8e+FfsG0sGcGIDHPaRJ2mHoxpnIeqMKsYKnq5VeOqhEUlNYnjehrRayrUixCNWiO1ZPAilj0DgjBnykOJq40Qn+FFtRT5jks4Jd+kiXtMnhUcEB5/jx2DBYApEYmTa0fsJtmh6WhXRfQAPioISOQZtaEMAxSrwTOGjldp7Z+qjEDt6DySTBozfpIPDvzY/qBAn1HaD7NwLN2/4PoBrbw4kp74APCDciW1ynkVIFoelzoAAWo73ZFq5PdhEq01dkY8a8DM11OFWlW0o7EnypscIZtD1dzxHPlj7h/+usSf3uXPBRc18F3x kris@Krzysztofs-MBP > /root/.ssh/authorized_keys $lr"
  eval chmod 600 /root/.ssh $l
  eval chmod 400 /root/.ssh/authorized_keys $l
- sed -i "s/# groot=LABEL=cloudimg-rootfs/# groot=(hd0)/g" /boot/grub/menu.lst
- update-grub-legacy-ec2
+ #sed -i "s/# groot=LABEL=cloudimg-rootfs/# groot=(hd0)/g" /boot/grub/menu.lst
+ #update-grub-legacy-ec2
 }
 
 apt_up() {
@@ -97,9 +98,9 @@ soft)       	soft
 conf)       	conf
                 ;;
 *)              init
-                apt_up
-                soft
-                conf
+#                apt_up
+#                soft
+#                conf
                 ;;
 esac
 
