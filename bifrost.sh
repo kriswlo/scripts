d12e787b1e1e213f7159e58296da7044e9b6701b
kriswlo <37249477+kriswlo@users.noreply.github.com>
2019-03-05 09:55:39 +0100

    Update bifrost.sh

--- bifrost.sh
+++ bifrost.sh
@@ -23,11 +23,11 @@ init() {
  eval "echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9Rv7mjUkhhKAXp/FORWCLoKtr3Wkp+CL8e+FfsG0sGcGIDHPaRJ2mHoxpnIeqMKsYKnq5VeOqhEUlNYnjehrRayrUixCNWiO1ZPAilj0DgjBnykOJq40Qn+FFtRT5jks4Jd+kiXtMnhUcEB5/jx2DBYApEYmTa0fsJtmh6WhXRfQAPioISOQZtaEMAxSrwTOGjldp7Z+qjEDt6DySTBozfpIPDvzY/qBAn1HaD7NwLN2/4PoBrbw4kp74APCDciW1ynkVIFoelzoAAWo73ZFq5PdhEq01dkY8a8DM11OFWlW0o7EnypscIZtD1dzxHPlj7h/+usSf3uXPBRc18F3x kris@Krzysztofs-MBP > /root/.ssh/authorized_keys $lr"
  eval chmod 600 /root/.ssh $l
  eval chmod 400 /root/.ssh/authorized_keys $l
- #sed -i "s/# groot=LABEL=cloudimg-rootfs/# groot=(hd0)/g" /boot/grub/menu.lst
- #update-grub-legacy-ec2
 }
 
 apt_up() {
+ sed -i "s/# groot=LABEL=cloudimg-rootfs/# groot=(hd0)/g" /boot/grub/menu.lst
+ update-grub-legacy-ec2
  export DEBIAN_FRONTEND=noninteractive
  export DEBIAN_PRIORITY=critical
  apt-get -qy update
